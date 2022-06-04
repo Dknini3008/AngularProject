e13e2c4844091135546c3e0d6c7a714181fd3d49
Dknini3008 <gominola3008@gmail.com>
2022-06-03 21:52:43 -0600

    MENU ITEMS

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  public menuOptions: any;
+
+  constructor( public apiService:ApiService, ) { }
 
   ngOnInit(): void {
+
+    this.menuOptions = this.apiService.getUsers();
+
   }
 
 }
